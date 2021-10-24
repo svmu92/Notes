e12a3337dd46d472b8e4ad17ec446d3b467e8218
svmu92 <sreenathv08@gmail.com>
2021-10-24 12:17:16 +0530

    Created repo and added some note files

---
+++ Interview/Company Interview Questions/rockwellques.c
@@ -0,0 +1,47 @@
+****************  Selection Procedure for ROCKWELL COLLINS*********************
+
+1.written round:
+-consisted of 50 ques, 25 from c,c++ (basics) & 25 from aptitude(RS aggarwal).
+ No negative marking.
+(candidates above 28 marks were shortlisted for the next round).
+
+2.Technical rounds:
+First : 
+a. what do you mean by static keyword used in class? 
+b. where are intialized & uninitialized static variables stored?
+c. what is DMA? explain in both c & c++.
+d. what is virtual keyword used for?
+e. what is run time polymorphism & explain by writing the code.
+f. define the layers in OSI model- their work & protocol used.(project based)
+g. Difference b\w a switch & a router ? In which layer of OSI are they used?
+h. What is interface? (java)
+i. How to avoid memory leak? 
+
+Second:
+a. write the code for preorder traversal of BST using recursion.
+b. difference between overriding & overloading?
+c. difference bw virtual &  pure virtual functions?
+d. write the code for reversing the linked list.
+e. puzzle: There are 8 balls, out of which 7 balls are of equal weight. you are given a balance to weight. In minimum, how many times can you 
+   know, which is the lighter one?
+  (Ans: 3)
+   (explanation: divide 8 balls in 2 halves.weigh them ,put aside the lighter 4. Divide these 4 balls into 2 halves, weigh them. Put aside the lighter 2.
+   Weigh each. Thus, lighter ball is found).
+f. puzzle: there are 2 jugs. one with 4l & one with 5l of capacity. how will you   get 3l ?
+  (Ans : fill 4l jug & pour it into 5l jug. Now, 5l jug has the capacity to hold 1l only. Again fill 4l jug & pour 1l into 5l jug. The remaining water in 4l
+   jug is 3l.)
+
+Third:(technical cum Hr)
+a. DIFFERENCE BW tcp & udp?
+b. write the code for sorting the string "Rockwellcollins" using quicksort.
+c. puzzle: There is a loaf of bread.I want to cut it into 8 pieces,by using the knife atmost 3 times.
+  (Ans: cut the loaf in 2 halves (knife used 1ce).put one slice on another.
+   again cut in 2 halves, it gives 4 slices(knife used twice). again,put the 2 
+   slices over the other 2. Again ,cut in 2 halves, it will give total 8 pieces
+   (knife used thrice)).
+d. Advantages of c over java?
+e. General HR questions.
+
+Thanks & regards
+Kanika
+b66k1.
